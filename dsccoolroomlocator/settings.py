6ccc6de901086de75679b403cd5170c43baeaef3
Divyansh Agarwal <divag.bvp@gmail.com>
2019-02-13 20:22:36 +0530

    minor changes

--- dsccoolroomlocator/settings.py
+++ dsccoolroomlocator/settings.py
@@ -89,8 +89,15 @@ WSGI_APPLICATION = 'dsccoolroomlocator.wsgi.application'
 DATABASES = {
     'default': {
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'roomlocatordb',
+        'USER':'postgres',
+        'PASSWORD':'12.divyansh',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
+
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
                 
     }
 }
--- roomlocator/admin.py
+++ roomlocator/admin.py
@@ -8,8 +8,8 @@ from django.apps import apps
 app = apps.get_app_config('roomlocator')
 
 class PersonAdmin(admin.ModelAdmin):
-    search_fields = ['room','number','isAvailable','day','time']
-
+    search_fields = ['room__number','block__name','room']
+    #list_filter = ['room','room__number__number','day__name','period__time']
 for model_name, model in app.models.items():
     model_admin = type(model_name + "Admin", (PersonAdmin, ), {'list_display': tuple([field.name for field in model._meta.fields])})
     admin.site.register(model, model_admin)
